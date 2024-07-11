f8491a976604bc340798e093dd0f57666b63c966
BahaaZidan <gebnatorky@gmail.com>
2024-07-11 04:29:58 +0300

    loggen-in

--- src/routes/(logged-in)/+layout.svelte
+++ src/routes/(logged-in)/+layout.svelte
@@ -14,7 +14,6 @@
 			</div>
 			<ul class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
 				<li><a href="/about">About</a></li>
-				<li><a href="/login">Login</a></li>
 				<li><button on:click={() => signOut()}>Logout</button></li>
 			</ul>
 		</div>
