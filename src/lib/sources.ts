1ccc3a5b0499b15013bec7c8188b10f8192ab48f
BahaaZidan <gebnatorky@gmail.com>
2024-07-27 20:53:16 +0300

    good old data entry

--- src/lib/sources.ts
+++ src/lib/sources.ts
@@ -1,1 +1,41 @@
-export const sources = ['https://gebna.gg/rss.xml', 'https://www.joshwcomeau.com/rss.xml'];
+export const sources = [
+	'https://gebna.gg/rss.xml',
+	'https://www.joshwcomeau.com/rss.xml',
+	'https://kentcdodds.com/blog/rss.xml',
+	'https://overreacted.io/atom.xml',
+	'https://www.robinwieruch.de/index.xml',
+	'https://deno.com/feed',
+	'https://go.dev/blog/feed.atom',
+	'https://the-guild.dev/feed.xml',
+	'https://moderncss.dev/feed/',
+	'https://schiener.io/feed.xml',
+	'https://css-tip.com/feed/feed.xml',
+	'https://astro.build/rss.xml',
+	'https://ishadeed.com/feed.xml',
+	'https://evilmartians.com/chronicles.atom',
+	'https://blog.scottlogic.com/atom.xml',
+	'https://effectivetypescript.com/atom.xml',
+	'https://css-tricks.com/feed/',
+	'https://frontendmasters.com/blog/feed/',
+	'https://atlassian.com/engineering/feed',
+	'https://engineering.fb.com/feed/',
+	'https://engineering.flipboard.com/feed.xml',
+	'https://www.digitalocean.com/community/tutorials.atom',
+	'https://www.eventbrite.com/engineering/feed/',
+	'https://www.intercom.com/blog/engineering/feed/',
+	// TODO: medium is a centrailzed service + medium's official feed only lists the last 10 posts. need a better solution
+	'https://medium.com/feed/@npayot',
+	'https://netflixtechblog.com/feed',
+	'https://developerblog.zendesk.com/feed',
+	'https://medium.com/feed/hootsuite-engineering',
+	'https://blog.flipkart.tech/feed',
+	// TODO: dev.to is a centralized service
+	'https://dev.to/feed/ryansolid',
+	// TODO: relying on feedburner should be retired in favor of scraping at build time (?)
+	'https://feeds.feedburner.com/blogspot/amDG',
+	'https://feeds.feedburner.com/brad-frosts-blog',
+	// TODO: youtube is a centralized service + youtube's official feed only lists the last 20 videos. need a better solution
+	'https://www.youtube.com/feeds/videos.xml?channel_id=UCsBjURrPoezykLs9EqgamOA',
+	// TODO: tumblr lol
+	'https://yahoodevelopers.tumblr.com/rss'
+];
